28bc1d1c2f5d4769567142378b05539f9d2dc6e2
Aerial LLC <110753825+e1uen@users.noreply.github.com>
2025-01-14 20:39:43 +0700

    Update service_install.bat

--- service_install.bat
+++ service_install.bat
@@ -39,7 +39,7 @@ if not defined selectedFile (
     goto :eof
 )
 
-:: Parsing args (mergeargs: 2=start wf|1=wf argument|0=default)
+:: Parsing args (mergeargs: 2=start param|1=params args|0=default)
 set "args="
 set "capture=0"
 set "mergeargs=0"
@@ -88,12 +88,7 @@ for /f "tokens=*" %%a in ('type "!selectedFile!"') do (
                     set "temp_args=!temp_args! !arg!"
                 )
 
-                if "!arg:~0,4!" EQU "--wf" (
-                    set "mergeargs=2"
-                ) else if "!arg!" EQU "--dpi-desync" (
-                    set "mergeargs=2"
-                ) else if "!arg!" EQU "--dpi-desync-fooling" (
-                    set "mergeargs=2"
+                    if "!arg:~0,2!" EQU "--" (
                 ) else if !mergeargs!==2 (
                     set "mergeargs=1"
                 )
